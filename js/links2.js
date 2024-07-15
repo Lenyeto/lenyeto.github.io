f98aa252a357deaaac42dee956a81c62e0eb9be2
Lenyeto <patg.lenyeto@gmail.com>
2024-07-14 22:58:25 -0400

    Updated page 2 links

---
+++ js/links2.js
@@ -0,0 +1,9 @@
+export const links = [ 
+	{ 
+		name: "Google", 
+		link: 
+"www.google.com", 
+		image: 
+"https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png", 
+	}
+];
