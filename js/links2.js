8c63641995eaed479737021d21e60060afe3667f
Lenyeto <patg.lenyeto@gmail.com>
2024-07-27 19:06:34 -0400

    Update links2.js

--- js/links2.js
+++ js/links2.js
@@ -2,7 +2,7 @@ export const links = [
 	{ 
 		name: "Google", 
 		link: 
-"www.google.com", 
+"https://www.google.com/", 
 		image: 
 "https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png", 
 	}
